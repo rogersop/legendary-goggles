52c535ab51d0f507045d269c5a3bef5668fd83ea
rogersop <paulerogerson@gmail.com>
2021-02-16 11:12:04 +0000

    fix eslint errors

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ class LegendaryGoggles {
     this.lenses = 2;
     this.visibility = 5;
   }
+
   cleanGoggles() {
+    // eslint-disable-next-line no-console
     console.log('Wipe, wipe...');
-    this.visibility++;
+    this.visibility += 1;
     this.condition = 10;
   }
 }
+
+module.exports = LegendaryGoggles;
