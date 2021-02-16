ea2565fbc7fd529a6662919bbb797524802aa63e
rogersop <paulerogerson@gmail.com>
2021-02-16 10:47:23 +0000

    add cleanGoggles method

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 class LegendaryGoggles {
   constructor() {
     this.lenses = 2;
+    this.visibility = 5;
+  }
+  cleanGoggles() {
+    console.log('Wipe, wipe...');
+    this.visibility++;
   }
 }
